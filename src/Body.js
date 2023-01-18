67a59652e2a3e5dd40e751512d5c8dad23f99c82
Jenna Moon <jenna0804@hotmail.com>
2023-01-18 03:02:11 -0800

    body incomplete

--- src/Body.js
+++ src/Body.js
@@ -1,52 +1,57 @@
 class Body {
-    constructor(el, segment) {
-      this.node = document.createElement('div');
-      this.node.setAttribute('class', 'body');
-      el.appendChild(this.node);
-      
-      this.previous = segment;
-      //this.currentDirection = this.previous.currentDirection;
-      this.SPEED = this.previous.speed;
-      this.node.style.top = this.previous.prevTop;
-      this.node.style.left = this.previous.prevLeft;
-      
-  
-      setTimeout(this.move.bind(this), this.SPEED);
-    }
-  
-    move() {
-      const body = this.node;
-      //const direction = this.currentDirection = this.previous.currentDirection;
-      
-      let topPosition = this.previous.prevTop;
-    //   Number(this.previous.node.style.top.replace('px', ''));
-      let leftPosition = this.previous.prevLeft;
-    //   Number(this.previous.node.style.left.replace('px', ''));
-      this.prevTop = topPosition;
-      this.prevLeft = leftPosition;  
+  constructor(el, arr, num) {
+    this.node = document.createElement('div');
+    this.node.setAttribute('class', 'body');
+    el.appendChild(this.node);
+    console.log('arr', arr);
+    this.position = arr;
+    this.num = num;
 
-      body.style.left = `${leftPosition}px`;
-      body.style.top = `${topPosition}px`;
-  
-      // position movements depending on direction
-    //   if (direction === 'right') {
-    //     body.style.left = `${(leftPosition += 50)}px`;
-    //     }
-      
-    //   if (direction === 'left') {
-    //     body.style.left = `${(leftPosition -= 50)}px`;
-    //     }
-      
-    //   if (direction === 'up') {
-    //     body.style.top = `${(topPosition -= 50)}px`;
-    //     }
-      
-    //   if (direction === 'down') {
-    //     body.style.top = `${(topPosition += 50)}px`;
-    //     }
-      
-  
-      setTimeout(this.move.bind(this), this.SPEED);
-    }
+    // this.node.style.top = arr[0][0]
+    // this.node.style.left = arr[0][1]
+
+    setTimeout(this.move.bind(this), 250);
   }
+
+  move() {
+    console.log(this.position[this.position.length - 1])
+    this.node.style.top = this.position[this.position.length - this.num][0];
+    this.node.style.left = this.position[this.position.length - this.num][1];
+    setTimeout(this.move.bind(this), 250);
+  }
+}
+
+
+// class Body {
+//     constructor(el, segment) {
+//       this.node = document.createElement('div');
+//       this.node.setAttribute('class', 'body');
+//       el.appendChild(this.node);
+      
+//       this.previous = segment;
+//       //this.currentDirection = this.previous.currentDirection;
+//       this.SPEED = this.previous.speed;
+//       this.node.style.top = this.previous.prevTop;
+//       this.node.style.left = this.previous.prevLeft;
+      
+  
+//       setTimeout(this.move.bind(this), this.SPEED);
+//     }
+  
+//     move() {
+//       const body = this.node;
+//       //const direction = this.currentDirection = this.previous.currentDirection;
+//       let topPosition = this.previous.prevTop;
+//     //   Number(this.previous.node.style.top.replace('px', ''));
+//       let leftPosition = this.previous.prevLeft;
+//     //   Number(this.previous.node.style.left.replace('px', ''));
+//       this.prevTop = topPosition;
+//       this.prevLeft = leftPosition;  
+
+//       body.style.left = `${leftPosition}px`;
+//       body.style.top = `${topPosition}px`;
+  
+//       setTimeout(this.move.bind(this), this.SPEED);
+//     }
+//   }
   
--- src/main.js
+++ src/main.js
@@ -9,25 +9,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const head = new Head(board);
   const apple = new Apple(board);
+  const trail = [];
+  let num = 1;
 
-  // console.log(`Apple Top: ${apple.node.style.top}, Apple Left: ${apple.node.style.left}`);
-  // console.log(`Snake Top: ${head.node.style.top}, Snake Left: ${head.node.style.left}`);
-  // setInterval(eat, speed);
-  let tail = head;
   function eat() {
     //check if the position of head and apple are the same
     if (head.node.style.top === apple.node.style.top && head.node.style.left === apple.node.style.left) {
       // send apple to random new position 
       apple.position();
       // create new body passing in el and previous segment
-      tail = new Body(board, tail);
+      // tail = new Body(board, tail);
+      // trail.push([head.node.style.top, head.node.style.left]);
+      setInterval(addBody, head.SPEED);
+      // let bodies = new Body(board, trail, num);
+      num += 1;
       score.textContent = `Score: ${++counter}`;
-      head.SPEED -= 10; //speed increases
-      console.log('speed', head.SPEED);
+      // head.SPEED -= 10; //speed increases
+      console.log('num', num);
     }
   }
 
   setInterval(eat, head.SPEED);
+
+  function addBody() {
+    trail.push([head.node.style.top, head.node.style.left]);
+    if (trail.length > counter) {
+      trail.shift();
+    }
+
+    document.querySelectorAll('.body').forEach(e => e.remove());
+
+    for (let i = 0; i < trail.length; i++) {
+      let what = document.createElement('div');
+      what.setAttribute('class', 'body');
+      board.appendChild(what);
+      what.style.top = trail[i][0]
+      what.style.left = trail[i][1]
+      
+
+    }
+    // console.log(trail[trail.length - 1])
+  }
 
   body.addEventListener('keydown', (e) => {
     if (e.code === 'ArrowLeft' && head.currentDirection !== 'right') {
